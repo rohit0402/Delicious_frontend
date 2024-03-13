f11a48955e7ec78a6586d389a6ac4cd11b50107f
Rohit Sonar <rohit456sonar@gmail.com>
2024-03-13 19:55:54 +0530

    updating navbar for more responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,149 +1,130 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearch } from "../../redux/slices/SearchSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import NavList from "./NavList";
 import axios from "axios";
-import { loginUser,setUser } from "../../redux/slices/AuthSlice";
+import { loginUser, setUser } from "../../redux/slices/AuthSlice";
 import { getCart } from "../../Helper";
 import { setCart } from "../../redux/slices/CartSlice";
 import toast from "react-hot-toast";
-axios.defaults.withCredentials=true;
+axios.defaults.withCredentials = true;
 
 function Navbar() {
   const dispatch = useDispatch();
 
   const [toggle, setToggle] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   const handleChange = (e) => {
     dispatch(setSearch(e.target.value));
   };
 
   const auth = useSelector((state) => state.auth.isAuth);
-  const user=useSelector((state)=> state.auth.user);
+  const user = useSelector((state) => state.auth.user);
+
   const getUser = async () => {
     try {
-      const res = await axios.get("https://delicious-backend-1.onrender.com/api/getUser", { withCredentials: true });
+      const res = await axios.get(
+        "https://delicious-backend-1.onrender.com/api/getUser",
+        { withCredentials: true }
+      );
       const data = res.data;
       dispatch(setUser(data.user));
       dispatch(loginUser());
-  
-      // Use await with getCart
+
       const cartData = await getCart(data.user);
       dispatch(setCart(cartData.cartItems));
     } catch (error) {
-      console.error('Error fetching user data:', error);
-      // Handle error
+      console.error("Error fetching user data:", error);
     }
   };
 
-  const handleLogout=async ()=>{
-    const res=await axios.get("https://delicious-backend-1.onrender.com/api/logout");
-    const data=await res.data;
+  const handleLogout = async () => {
+    const res = await axios.get(
+      "https://delicious-backend-1.onrender.com/api/logout"
+    );
+    const data = await res.data;
     toast.success(data.message);
-    window.location.href="/";
-  }
+    window.location.href = "/";
+  };
+  const handleClick = () => {
+    if (windowWidth < 767) {
+      setToggle(!toggle);
+    }
+  };
+  useEffect(() => {
+    getUser();
 
-useEffect(()=>{
-  getUser();
-},[]);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <div className="fixed top-0 w-[100vw]">
-      <nav className="bg-yellow-400 shadow-lg ">
-        <div className="max-w-70xl  px-4 sm:px-6 lg:px-8">
+    <div className="fixed top-0 w-full">
+      <nav className="bg-yellow-400 shadow-lg">
+        <div className="max-w-70xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
-            {/* Logo toggle */}
-            <div className="flex-shrink-0 flex items-center mt-2.5 mr-1.5">
-              <h3 className={`absolute top-5 left-5 w-auto h-9 cursor-pointer text-lg transition-all ease-in-out duration-500 ${
-                  toggle && "hidden"
-                }`}
-                onClick={() => {
-                  if (window.innerWidth < 768) {
-                    setToggle(true);
-                  }
-                }}><i><b>DELICIOUS</b></i></h3>
-              {/* <img
-                className={`absolute top-4.9 left-5 w-auto h-9 cursor-pointer transition-all ease-in-out duration-500 ${
-                  toggle && "hidden"
-                }`}
-                onClick={() => {
-                  if (window.innerWidth < 768) {
-                    setToggle(true);
-                  }
-                }}
-                src="/src/assets/logo.jpg"
-                alt="Restaurant Logo"
-              /> */}
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={`absolute top-4.9 left-6 w-auto h-9 lg:hidden cursor-pointer transition-all ease-in-out duration-500 ${
-                  !toggle && "hidden"
-                }`}
-                onClick={() => setToggle(false)}
-              />
+            <div className="flex items-center mr-3">
+              {/* Logo */}
+              <h3 className="text-lg font-bold sm:mr-5" onClick={handleClick}>
+                <Link to="/">
+                  {!toggle ? (
+                    <b>
+                      <i className=" font-serif font-bold">DELICIOUS</i>
+                    </b>
+                  ) : (
+                    <FontAwesomeIcon className="text-3xl" icon={faTimes} />
+                  )}
+                </Link>
+              </h3>
             </div>
-            <NavList toggle={toggle} setToggle={setToggle} auth={auth} />
-            {/* Navigation Links */}
-            {window.innerWidth > 767 && (
-              <div className="flex items-center justify-center ml-5 m-0 flex-1 sm:items-stretch sm:justify-start">
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex">
-                    <Link
-                      to="/"
-                      className="px-3 py-2 mt-3 text-black text-lg font-bold leading-5 hover:text-black-300 focus:outline-none focus:text-gray-100 transition duration-150 ease-in-out"
-                    >
-                      Home
-                    </Link>
-                    <Link
-                      to="/"
-                      className="ml-2 px-3 mt-3 py-2 text-black text-lg font-bold leading-5 hover:text-black-300 focus:outline-none focus:text-gray-100 transition duration-150 ease-in-out"
-                    >
-                      Menu
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            )}
 
-            {/* searchbar */}
-            <div className="flex items-center justify-center m-auto  flex-1 sm:py-4 sm:px ">
-              <div className="flex items-center">
+            {/* Search */}
+            {(windowWidth > 767 || toggle) && (
+              <div className="flex justify-start items-start  ">
                 <input
                   type="text"
                   placeholder="Search"
-                  className="m-auto ml-5 rounded-xl bg-white py-2 px-4 border border-gray-300 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                  className="w-full rounded-xl mt-2.5 bg-white py-2 px-4 border border-gray-300 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
                   onChange={handleChange}
                 />
               </div>
-            </div>
-
-            {/* auth */}
-            {window.innerWidth > 767 && auth && (
-              <div className="hidden sm:block sm:mr-auto">
-                <div className="flex items-center">
-                  <Link to="/" className="mt-4 mr-5 text-black text-lg font-semibold hover:cursor-pointer hover:underline  hover:text-black-300" onClick={handleLogout}>logout</Link>
-                </div>
-              </div>
             )}
 
-            {window.innerWidth > 767&&!auth && (
-              <div className="hidden sm:block sm:mr-auto">
-                <div className="flex items-center">
+            {/* Auth Links */}
+            {(windowWidth > 767 || toggle) && (
+              <div className="flex items-center">
+                {auth ? (
                   <Link
-                    to="/login"
-                    className="ml-4 mr-3 mt-4 text-black text-lg font-semibold hover:text-black-300"
+                    to="/"
+                    className="text-lg font-semibold ml-4 mr-5 cursor-pointer hover:text-black-300"
+                    onClick={handleLogout}
                   >
-                    Login
+                    Logout
                   </Link>
-                  <Link
-                    to="/signup"
-                    className="ml-2 mt-4 mr-5 text-black text-lg font-semibold hover:text-black-300"
-                  >
-                    Sign Up
-                  </Link>
-                </div>
+                ) : (
+                  <>
+                    <Link
+                      to="/login"
+                      className="text-lg font-semibold ml-4 mr-3 cursor-pointer hover:text-black-300"
+                    >
+                      Login
+                    </Link>
+                    <Link
+                      to="/signup"
+                      className="text-lg font-semibold mr-5 cursor-pointer hover:text-black-300"
+                    >
+                      SignUp
+                    </Link>
+                  </>
+                )}
               </div>
             )}
           </div>
